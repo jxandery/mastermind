7078d1e49a72bb7de020a1819bea0593ccd6a8b5
Jack Yeh <jack.yeh@gmail.com>
2015-03-02 21:05:37 -0700

    added start method

---
+++ cli.rb
@@ -0,0 +1,25 @@
+require './printer'
+require './game'
+
+class CLI
+  attr_reader :command, :printer, :output_stream, :input_stream
+
+  def initialize(input_stream, output_stream)
+    @command = ""
+    @printer = Printer.new
+    @input_stream = input_stream
+    @output_stream = output_stream
+  end
+
+  def start
+    printer.introduction
+    until finished?
+      printer.command_request
+      @command = get_input
+      process_initial_commands
+    end
+    printer.ending
+  end
+
+end
+
